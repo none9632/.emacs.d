125aeb83893158c6fa39651eedcf54a91e8a714c
none9632 <none9632@protonmail.com>
2024-07-01 13:56:58 +0300

    init: update 'Speed up startup' section

--- init.el
+++ init.el
@@ -1,46 +1,26 @@
 ;; -*- lexical-binding: t no-byte-compile: t -*-
 
-(defvar centaur-gc-cons-threshold (if (display-graphic-p) 16000000 1600000)
-  "The default value to use for `gc-cons-threshold'. If you experience freezing,
-decrease this. If you experience stuttering, increase this.")
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
 
-(defvar centaur-gc-cons-upper-limit (if (display-graphic-p) 400000000 100000000)
-  "The temporary value for `gc-cons-threshold' to defer it.")
+;; Prevent flashing of unstyled modeline at startup
+(setq-default mode-line-format nil)
 
-(defvar centaur-gc-timer (run-with-idle-timer 10 t #'garbage-collect)
-  "Run garbarge collection when idle 10s.")
+;; Don't pass case-insensitive to `auto-mode-alist'
+(setq auto-mode-case-fold nil)
 
-(defvar default-file-name-handler-alist file-name-handler-alist)
-
-(setq file-name-handler-alist nil)
-(setq gc-cons-threshold centaur-gc-cons-upper-limit
-      gc-cons-percentage 0.5)
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            "Restore defalut values after startup."
-            (setq file-name-handler-alist default-file-name-handler-alist)
-            (setq gc-cons-threshold centaur-gc-cons-threshold
-                  gc-cons-percentage 0.1)
-
-            ;; GC automatically while unfocusing the frame
-            ;; `focus-out-hook' is obsolete since 27.1
-            (if (boundp 'after-focus-change-function)
-                (add-function :after after-focus-change-function
-                              (lambda ()
-                                (unless (frame-focus-state)
-                                  (garbage-collect))))
-              (add-hook 'focus-out-hook 'garbage-collect))
-
-            ;; Avoid GCs while using `ivy'/`counsel'/`swiper' and `helm', etc.
-            ;; @see http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
-            (defun my/minibuffer-setup-hook ()
-              (setq gc-cons-threshold centaur-gc-cons-upper-limit))
-
-            (defun my/minibuffer-exit-hook ()
-              (setq gc-cons-threshold centaur-gc-cons-threshold))
-
-            (add-hook 'minibuffer-setup-hook #'my/minibuffer-setup-hook)
-            (add-hook 'minibuffer-exit-hook #'my/minibuffer-exit-hook)))
+(unless (or (daemonp) noninteractive init-file-debug)
+  ;; Suppress file handlers operations at startup
+  ;; `file-name-handler-alist' is consulted on each call to `require' and `load'
+  (let ((old-value file-name-handler-alist))
+    (setq file-name-handler-alist nil)
+    (set-default-toplevel-value 'file-name-handler-alist file-name-handler-alist)
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                "Recover file name handlers."
+                (setq file-name-handler-alist
+                      (delete-dups (append file-name-handler-alist old-value))))
+              101)))
 
 ;; Hide cursor and mode-line at startup
 (setq-default mode-line-format nil)
